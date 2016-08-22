2ac71e08df0dfbd3f52b147ad856a84c1e0373fd
Vivian Esquivias <v.esquivias@gmail.com>
2016-08-22 12:21:42 +0200

    Updated comic book model

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -9,8 +9,24 @@ namespace ComicBookGallery.Models
     {
         public int Id { get; set; }
         public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
         public string DescriptionHtml { get; set; }
-        public Artist[] artist { get; set; }
+        public Artist[] Artist { get; set; }
         public bool Favorite { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
+
+        //series-title-issuenumber.jpg
+        public string CoverImageFileName{
+            get {
+                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg"; 
+            }
+        }
     }
 }
