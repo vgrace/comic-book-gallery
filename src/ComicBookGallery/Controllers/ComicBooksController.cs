94b348439e17e58010694b28cf188923bdd6c1aa
Vivian Esquivias <v.esquivias@gmail.com>
2016-08-22 12:35:40 +0200

    Updated comic book detail view to be strongly typed

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ComicBookGallery.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,20 +11,20 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-
-            ViewBag.SeriesTitle = "The Amazing Spider-Man";
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.Artists = new string[]
-            {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
-            };
-
-            return View();             
+            var comicBook = new ComicBook() {
+                SeriesTitle = "The Amazing Spider-Man", 
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new  Artist[]{
+                    new Artist { Name = "Script", Role = "Dan Slott"},
+                    new Artist { Name = "Pencils", Role = "Humberto Ramos"},
+                    new Artist { Name = "Inks", Role = "Victor Olazaba"},
+                    new Artist { Name = "Colors", Role = "Edgar Delgado"},
+                    new Artist { Name = "Letters", Role = "Chris Eliopoulos"}
+                }
+            }; 
+            
+            return View(comicBook);             
         }
        
     }
--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -11,7 +11,7 @@ namespace ComicBookGallery.Models
         public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
         public string DescriptionHtml { get; set; }
-        public Artist[] Artist { get; set; }
+        public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
 
         public string DisplayText
